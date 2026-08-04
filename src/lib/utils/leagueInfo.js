70709632f7e5d5e764aae5b93a383634deecd8c9
doctorcgs16 <122510960+doctorcgs16@users.noreply.github.com>
2026-08-03 22:29:57 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1316944743216709632"; // your league ID
+export const leagueID = "1204290981751435264"; // your league ID
 export const leagueName = "EF Taxi Squad"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
